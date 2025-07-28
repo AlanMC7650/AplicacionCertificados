3af05ea413cb9e39268d5610e80702ffa910e77f
Diego Andres <andresguti2003@gmail.com>
2025-07-28 14:17:23 -0600

    Verificando el archivo Estudiantes.html

--- insertar_usuarios.py
+++ insertar_usuarios.py
@@ -50,6 +50,6 @@ if __name__ == "__main__":
         contrasena_plana="alan5",
         documento="admin",
         pais_origen="Bolivia",
-        id_rol=1
+        id_rol=1,
     )
     conn.close()
