ec2f61b0e105472e55029dc6575183cc1dd5ae99
laboratorio <167577922+AlejandLeo@users.noreply.github.com>
2025-08-01 01:55:42 -0400

    CRUD Usuarios:Estudiante/Ponente Listo

--- app/api/usuarios/routes.py
+++ app/api/usuarios/routes.py
@@ -5,8 +5,8 @@ from app.controllers import u_controller as est
 from flask_login import login_user, logout_user, login_required, current_user
 from app.api.auth.utils import role_required
 
-# --- PARA COORDINADOR ---
-## Estudiantes
+# ----- PARA COORDINADOR -----
+## ---Estudiantes
 @usuario_bp.route("/coor/estudiantes", methods=["POST"])
 @login_required
 def crear_estudiante():
@@ -67,11 +67,75 @@ def borrar_estudiante(id_usuario):
 
 
 
-## Ponentes
+## ---Ponentes
 @usuario_bp.route("coor/expositores", methods=["GET"])
 @login_required
 def expositores():
     return render_template("Coordinador/partials/expositores.html",expositores=est.obtener_usuarios(2))
+
+@usuario_bp.route("/coor/expositores", methods=["POST"])
+@login_required
+def crear_expositor():
+    data = request.json
+    campos = ["nombre", "apellido", "email", "contrasena", "documento", "pais_origen"]
+    if not data or not all(k in data for k in campos):
+        return jsonify({"error": "Datos incompletos"}), 400
+    usu.crear_ponente(
+        data["nombre"],
+        data["apellido"],
+        data["email"],
+        data["contrasena"],
+        data["documento"],
+        data["pais_origen"],
+    )
+    return jsonify({"mensaje": "Ponente creado", "success": True}), 201
+
+@usuario_bp.route("/coor/expositores", methods=["GET"])
+@login_required
+def obtener_ponente():
+    return render_template("Coordinador/partials/expositores.html", expositores=usu.obtener_usuarios(rol=2))
+
+@usuario_bp.route('/coor/expositores/info/<int:id_u>',methods=['GET'])
+def obtener_datos_ponente_json(id_u):
+    ponente = usu.obtener_usuarios_id(2,id_u)
+    if ponente:
+        return jsonify(ponente[0])
+    return jsonify({}), 404
+
+@usuario_bp.route("/coor/expositores/<int:id_usuario>", methods=["PUT"])
+@login_required
+def editar_ponente(id_usuario):
+    data = request.json
+    campos_requeridos = ["nombre", "apellido", "email", "documento", "pais_origen", "id_rol"]
+    if not data or not all(k in data for k in campos_requeridos):
+        return jsonify({"error": "Datos incompletos"}), 400
+    contrasena = data.get("contrasena")
+    usu.actualizar_ponente(
+        id_usuario,
+        data["nombre"],
+        data["apellido"],
+        data["email"],
+        contrasena,
+        data["documento"],
+        data["pais_origen"],
+        data["id_rol"],
+    )
+    return jsonify({"mensaje": "Ponente actualizado"})
+
+@usuario_bp.route("/coor/expositores/<int:id_usuario>", methods=["DELETE"])
+@login_required
+def borrar_ponente(id_usuario):
+    try:
+        usu.eliminar_ponente(id_usuario)
+        return jsonify({"mensaje": "Ponente eliminado"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+
+
+
+
 
 
 # --- DEMAS ---
--- app/controllers/u_controller.py
+++ app/controllers/u_controller.py
@@ -1,7 +1,7 @@
 import psycopg2
 from app.db_c import get_connection
 from werkzeug.security import generate_password_hash
-
+# --- Usuarios General
 def obtener_usuarios(rol):
     conn = get_connection()  # conecta a la base de datos
     cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
@@ -18,6 +18,7 @@ def obtener_usuarios_id(rol,id):
     conn.close()  # cierra la conexión
     return rows  # devuelve los datos a quien haya llamado esta función
 
+# --- Usuarios Estudiantes
 def obtener_estudiantes():
     conn = get_connection()  # conecta a la base de datos
     cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
@@ -77,7 +78,7 @@ def crear_estudiantes_bulk(lista_estudiantes):
     try:
         conn = get_connection()
         cursor = conn.cursor()
-        id_rol = 1  # Por defecto: estudiante
+        id_rol = 3  # Por defecto: estudiante
         for estudiante in lista_estudiantes:
             cursor.execute(
                 """
@@ -104,16 +105,91 @@ def crear_estudiantes_bulk(lista_estudiantes):
         if conn:
             conn.close()
 
-"""
-def actualizar_estudiante(id_usuario, nombre, apellido, email, contrasena, documento, pais_origen):
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("UPDATE Usuarios SET nombre = %s, apellido = %s, email = %s,contrasena = %s,documento = %s,pais_origen = %s WHERE id_usuario = %s",
-                   (nombre, apellido, email, contrasena, documento, pais_origen, id_usuario,))
-    conn.commit()
-    conn.close()"""
-
 def eliminar_estudiante(id_usuario):
+    try:
+        conn = get_connection()
+        cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cursor.execute("DELETE FROM Usuarios WHERE id_usuario = %s", (id_usuario,))
+        conn.commit()
+        conn.close()
+    except psycopg2.Error as e:
+        conn.rollback()
+        return {"status": "error", "mensaje": "Error al eliminar: " + str(e)}
+
+
+# --- Usuarios Expositor
+
+def actualizar_ponente(
+    id_usuario, nombre, apellido, email, contrasena, documento, pais_origen, id_rol
+):
+    query = """
+        UPDATE Usuarios
+        SET nombre = %s, apellido = %s, email = %s,
+            documento = %s, pais_origen = %s, id_rol = %s
+        WHERE id_usuario = %s
+    """
+    values = [nombre, apellido, email, documento, pais_origen, id_rol, id_usuario]
+
+    if contrasena and contrasena.strip() != "":
+        query = """
+            UPDATE Usuarios
+            SET nombre = %s, apellido = %s, email = %s,
+                contrasena = %s, documento = %s, pais_origen = %s, id_rol = %s
+            WHERE id_usuario = %s
+        """
+        hashed = generate_password_hash(contrasena)
+        values = [nombre, apellido, email, hashed, documento, pais_origen, id_rol, id_usuario]
+    conn = get_connection()
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cursor.execute(query, values)
+    conn.commit()
+    conn.close()
+
+def crear_ponente(nombre, apellido, email, contrasena, documento, pais_origen):
+    conn = get_connection()
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    id_rol = 2  # Por defecto: ponente
+    hashed = generate_password_hash(contrasena)
+    cursor.execute(
+        "INSERT INTO Usuarios (nombre, apellido, email, contrasena, documento, pais_origen, id_rol) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+        (nombre, apellido, email, hashed, documento, pais_origen, id_rol),
+    )
+    conn.commit()
+    conn.close()
+
+def crear_ponentes_bulk(lista_expositores):
+    conn = None
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+        id_rol = 2  # Por defecto: ponentes
+        for expositor in lista_expositores:
+            cursor.execute(
+                """
+                INSERT INTO Usuarios (nombre, apellido, email, contrasena, documento, pais_origen, id_rol)
+                VALUES (%s, %s, %s, %s, %s, %s)
+            """,
+                (
+                    expositor["nombre"],
+                    expositor["apellido"],
+                    expositor["email"],
+                    expositor["contrasena"],
+                    expositor["documento"],
+                    expositor["pais_origen"],
+                    id_rol
+                ),
+            )
+        conn.commit()
+    except Exception as e:
+        print("Error al crear ponentes:", e)
+        if conn:
+            conn.rollback()
+        raise
+    finally:
+        if conn:
+            conn.close()
+
+def eliminar_ponente(id_usuario):
     try:
         conn = get_connection()
         cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
